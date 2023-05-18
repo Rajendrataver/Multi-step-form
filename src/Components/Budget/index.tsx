b6321319ea21da71a8779ed41691d86ac138e031
rajendra <rajendra.taver@encoresky.com>
2023-05-18 11:10:45 +0530

    fixed some issues and errors

--- src/Components/Budget/index.tsx
+++ src/Components/Budget/index.tsx
@@ -13,7 +13,8 @@ const Budget = ({budget,budgetError,setBudget}:budgetInterface) => {
             <h4>What’s your project budget?</h4>
            <p>Please select the project budget range you have in mind.</p>
            <span className='error'>{budgetError}</span>
-            <div className="input-fields-container">
+           <div className="input-fields-container">
+               
                <div className='budget-item'><input type="radio" name='budget' value={"$5.000 - $10.000"} checked={budget==='$5.000 - $10.000'} onChange={e=>setBudget(e)} /><label htmlFor="service">$5.000 - $10.000</label></div>
                <div className='budget-item'><input type="radio" name='budget' value={"$10.000 - $20.000"} checked={budget==='$10.000 - $20.000'} onChange={e=>setBudget(e)}/><label htmlFor="service">$10.000 - $20.000</label></div>
                <div className='budget-item'><input type="radio" name='budget' value={"$20.000 - $50.000"} checked={budget==='$20.000 - $50.000'} onChange={e=>setBudget(e)}/><label htmlFor="service">$20.000 - $50.000</label></div>
--- src/Components/Stepper/index.tsx
+++ src/Components/Stepper/index.tsx
@@ -2,37 +2,27 @@ import './index.css'
 const Stepper = ({ step }: { step: number }) => {
     console.log(step);
     switch (step) {
-      case 1:
+        case 1:
         return  <div className="stepper">
               <img src="assets/images/nav1.png" alt="" />
               </div>
-        break;
-      case 2:
+        case 2:
         return <div className="stepper">
           <img src="assets/images/nav2.png" alt="" />
         </div>
-        break;
-      case 3:
+        case 3:
         return <div className="stepper">
           <img src="assets/images/nav3.png" alt=""/>
         </div>
-        break;
         case 4:
         return <div className="stepper">
           <img src="assets/images/nav4.png" alt=""  />
         </div>
-        break;
-      default:
-return  <div className="stepper">
+        default:
+        return  <div className="stepper">
               <img src="assets/images/nav1.png" alt="" />
               </div>
-        
-        break;
     }
-    return (<>
-        
-       
-    </>);
 }
  
 export default Stepper;
